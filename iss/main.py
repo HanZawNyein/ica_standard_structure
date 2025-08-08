8bf23d4d5d1fb7215516c6c21d28bf79fa177a67
HanZawNyein <hanzawnyineonline@gmail.com>
2025-08-08 11:10:17 +0630

    [ADD] creating folder structure

--- iss/main.py
+++ iss/main.py
@@ -36,4 +36,4 @@ if __name__ == "__main__":
     path = "/home/agga/Documents/odoo-dev/ica_standard_structure/test"
     version = 18.0
     python = 3.10
-    create(path, version, python)
+    create_all(path, version, python)
